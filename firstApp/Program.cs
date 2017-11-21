064b47947122b29fcca813ef1a654646ddbf24f5
Gaurav Madaan <gaurav.madaan@technossus.com>
2017-11-21 09:29:54 +0530

    Json Serialization and DeSerialization

---
+++ firstApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace firstApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var product = new Product{
+                Id = 1,
+                Name ="Earphones",
+                Price = 500
+            };
+
+            var jsonString = JsonConvert.SerializeObject(product);
+
+            Console.WriteLine(jsonString);
+
+            var productFromJson = JsonConvert.DeserializeObject<Product>(jsonString);
+
+            Console.WriteLine(string.Format("The product Name is {0}", productFromJson.Name));
+
+            Console.Read();
+
+        }
+    }
+
+    public class Product{
+        public int Id{get;set;}
+        public string Name{get;set;}
+        public int Price{get;set;}
+    }
+}
